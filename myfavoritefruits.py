1f98e94c4a8c53339dbca9f255ad3284189bf6e2
Valentina <valentina.smarty@gmail.com>
2021-08-11 15:40:40 -0700

    My favorite fruits

---
+++ myfavoritefruits.py
@@ -0,0 +1,6 @@
+my_favorite_fruits = ['Lemon', 'Guava', 'Peach', 'Plum']o
+m = input('Favorite fruit? Ex. Pear\n')
+if m in my_favorite_fruits:
+  print('That is one of my favorite fruits!')
+else:
+  print('That is not a fruit that I like.')
