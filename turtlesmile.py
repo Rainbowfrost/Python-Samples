82cb880ad14d2795470cc9e6f6c01360342e88ba
Valentina <valentina.smarty@gmail.com>
2021-10-24 14:20:48 -0700

    Smiley face with Turtle

---
+++ turtlesmile.py
@@ -0,0 +1,26 @@
+import turtle
+my_pen = turtle.Turtle()      
+
+def eye(rad):
+    my_pen.down()
+    my_pen.circle(rad)
+    my_pen.up()
+
+my_pen.fillcolor('blue')
+my_pen.begin_fill()
+my_pen.circle(100)
+my_pen.end_fill
+my_pen.up
+
+my_pen.goto(-40, 120)
+my_pen.down
+my_pen.circle(10)
+my_pen.up
+
+my_pen.goto(40, 120)
+my_pen.down
+my_pen.circle(10)
+my_pen.up
+
+my_pen.goto(0, 0)
+my_pen.down
